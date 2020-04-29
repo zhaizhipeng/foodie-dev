5d5f9dc52f5b9b7405acb91607f91a669ae2dfd4
zhaizhipeng <zhaizhipeng@extracme.com>
2020-04-29 14:40:33 +0800

    整合 lombok

--- foodie-dev-pojo/src/main/java/com/ysdrzp/bo/UserBo.java
+++ foodie-dev-pojo/src/main/java/com/ysdrzp/bo/UserBo.java
@@ -2,52 +2,21 @@ package com.ysdrzp.bo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
-/**
- * 用户注册传入参数对象
- */
 @ApiModel("用户注册传入参数对象")
+@Data
 public class UserBo {
 
-    /**
-     * 用户名
-     */
     @ApiModelProperty(value = "用户名", name = "username", required = true, example = "ysdrzp")
     private String username;
 
-    /**
-     * 密码
-     */
+
     @ApiModelProperty(value = "密码", name = "password", required = true, example = "123456")
     private String password;
 
-    /**
-     * 确认密码
-     */
+
     @ApiModelProperty(value = "确认密码", name = "checkPassword", required = true, example = "123456")
     private String checkPassword;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCheckPassword() {
-        return checkPassword;
-    }
-
-    public void setCheckPassword(String checkPassword) {
-        this.checkPassword = checkPassword;
-    }
 }
